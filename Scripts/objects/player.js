ba13cb0ac118b23653e521156cd58943f84250e6
trashpanda95 <aubrian230@gmail.com>
2018-01-04 20:37:28 -0500

    Add files via upload

--- Scripts/objects/player.js
+++ Scripts/objects/player.js
@@ -34,7 +34,7 @@ var objects;
             this.regXY();
             this.y = 400;
             this.x = 400;
-            this.playerHealth = 50;
+            this.playerHealth = 100;
             this.keyBoardKey = new managers.keyBoardInput();
             this.bulletSpawn = new createjs.Point(this.y - 35, this.x);
         };
